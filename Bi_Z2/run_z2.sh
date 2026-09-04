b3784acdf2eff7d1e8ea97026f0d18e75a0b70cf
Hyun-Jung Kim <Infant@kias.re.kr>
2026-09-04 16:29:40 +0900

    Fix Bi Z2 Fortran WAVECAR path

--- Bi_Z2/run_z2.sh
+++ Bi_Z2/run_z2.sh
@@ -62,8 +62,10 @@ mkdir -p "$result_dir"
 cd "$result_dir"
 
 set +e
+# The legacy Fortran CLI parses -f with list-directed input, where a leading
+# slash terminates the value. Use this relative path instead of $wavecar.
 "$build_dir/vaspberry" \
-  -f "$wavecar" -o NFIELD -z2 1 \
+  -f ../WAVECAR -o NFIELD -z2 1 \
   -kx 12 -ky 12 -s 2 -ii 1 -if 10 \
   >fortran.log 2>&1
 fortran_status=$?
